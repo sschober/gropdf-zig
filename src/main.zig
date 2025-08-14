536ac2df1bbdee5919c75c86fea84232f7911418
Sven Schober <sv3sch@gmail.com>
2025-08-14 07:56:03 +0200

    add second line to text
    this required a change in the way the text object was
    positioned. before, we had the position outside the
    text object, now it is inside using the text matrix
    command Tm.
    
    we also have to set the text leading by using the
    TL command. Without this, the next line operator
    T* would only bring the cursor back but not down.

--- src/main.zig
+++ src/main.zig
@@ -5,7 +5,7 @@ const lib = @import("pdf.zig");
 
 pub fn main() !void {
     var pdfDoc = try lib.PdfDocument.new();
-    _ = try pdfDoc.addPage("1. 0. 0. 1. 50. 700. cm\nBT\n  /F0 36. Tf\n  (Hello, World!) Tj \nET\n");
+    _ = try pdfDoc.addPage("BT\n/F0 36. Tf\n1 0 0 1 120 700 Tm\n50 TL\n(Hello, World!) Tj T*\n/F0 12. Tf\n(This is a second sentence!) Tj\nET\n");
     const writer = std.io.getStdOut().writer();
     try pdfDoc.print(writer);
 }
