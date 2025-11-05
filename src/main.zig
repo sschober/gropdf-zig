c3a4a6c77ee68687066b1950e5ad53b7f5f7edb3
Sven Schober <sv3sch@gmail.com>
2025-11-05 15:25:40 +0100

    main: remove unneeded `handle_font_cmd()`

--- src/main.zig
+++ src/main.zig
@@ -7,33 +7,6 @@ const String = []const u8;
 const Allocator = std.mem.Allocator;
 const Transpiler = @import("Transpiler.zig");
 
-fn handle_font_cmd(
-    allocator: Allocator,
-    font_map: *std.AutoHashMap(usize, usize),
-    font_glyph_widths_maps: *std.AutoHashMap(usize, groff.GlyphMap),
-    doc: *pdf.Document,
-    font_name: String,
-    font_num: usize,
-) !void {
-    var pdf_font_num: usize = 0;
-    if (std.mem.eql(u8, "TR", font_name)) {
-        pdf_font_num = try doc.addStandardFont(pdf.StandardFonts.Times_Roman);
-    } else if (std.mem.eql(u8, "TB", font_name)) {
-        pdf_font_num = try doc.addStandardFont(pdf.StandardFonts.Times_Bold);
-    } else if (std.mem.eql(u8, "TI", font_name)) {
-        pdf_font_num = try doc.addStandardFont(pdf.StandardFonts.Times_Italic);
-    } else if (std.mem.eql(u8, "CR", font_name)) {
-        pdf_font_num = try doc.addStandardFont(pdf.StandardFonts.Courier);
-    } else {
-        std.debug.print("warning: unsupported font: {s}\n", .{font_name});
-        return;
-    }
-    std.debug.print("adding {s} as pdf font num {d} to font map\n", .{ font_name, pdf_font_num });
-    try font_map.put(font_num, pdf_font_num);
-    const glyph_map = try groff.readGlyphMap(allocator, font_name);
-    try font_glyph_widths_maps.put(pdf_font_num, glyph_map);
-}
-
 /// reads groff output (groff_out(5)) and produces a PDF 1.1 compatible file
 /// reads from stdin and writes to stdout, takes no arguments ATM
 pub fn main() !u8 {
