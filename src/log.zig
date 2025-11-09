7db3cabf03f4dcb3a9aff78867fc4f1aa45d2bba
Sven Schober <sv3sch@gmail.com>
2025-11-09 15:31:35 +0100

    log: introduce new log function `info()` and comment others

--- src/log.zig
+++ src/log.zig
@@ -3,18 +3,26 @@ const std = @import("std");
 pub var is_debug: bool = false;
 pub var is_warn: bool = false;
 
+/// log unconditionally
+pub fn info(comptime fmt: []const u8, args: anytype) void {
+    std.debug.print(fmt, args);
+}
+
+/// only log, if debugging is enabled
 pub fn dbg(comptime fmt: []const u8, args: anytype) void {
     if (is_debug) {
         std.debug.print(fmt, args);
     }
 }
 
+/// only log, if warning is enbaled
 pub fn warn(comptime fmt: []const u8, args: anytype) void {
     if (is_warn) {
         std.debug.print(fmt, args);
     }
 }
 
+/// log error unconditionally
 pub fn err(comptime fmt: []const u8, args: anytype) void {
     std.debug.print(fmt, args);
 }
